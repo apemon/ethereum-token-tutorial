120d03803f6518d82b266cffef3ef5eb5064f9bf
Parin Chiamananthapong <apemon642002@hotmail.com>
2017-08-23 22:56:36 +0700

    update web service

--- src/app.js
+++ src/app.js
@@ -8,6 +8,18 @@ var port = process.env.PORT || 3000;
 var app = express();
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
+var token;
+// setup contract
+var Token = contract(tokenContract);
+Token.setProvider(web3.currentProvider);
+// initialize contract
+Token.deployed().then(function(instance){
+    token = instance;
+    app.listen(port, function () {
+        console.log('Example app listening on port ' + port)
+    });
+});
+
 app.get('/', function (req, res) {
     res.send('hello world');
 });
@@ -18,13 +30,35 @@ app.get('/listAccount', function (req, res) {
     res.send(accounts);
 });
 
-app.get('/account/:addr', function (req,res) {
-    var balance = {};
-    var eth_balance = web3.eth.getBalance(req.params.addr);
-    balance.eth = web3.fromWei(eth_balance, 'ether').toString(10);
-    res.send(balance);
+app.get('/transfer',function(req, res){
+    var from = req.query.from;
+    var to = req.query.to;
+    var amount = req.query.amount;
+    token.transfer(to, amount, {from: from}).then(function(v){
+        res.send(v);
+    }).catch(function(err){
+        res.send(err);
+    });
 });
 
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port)
+app.get('/freeze',function(req, res){
+    var target = req.query.target;
+    var from = req.query.from;
+    token.freezeAccount(target, true, {from:from}).then(function(v){
+        res.send(v);
+    }).catch(function(err){
+        res.send(err);
+    });
+});
+
+app.get('/account/:addr', function (req,res) {
+    var addr = req.params.addr;
+    var balance = {};
+    var eth_balance = web3.eth.getBalance(addr);
+    balance.eth = web3.fromWei(eth_balance, 'ether').toString(10);
+    // get token balance
+    token.balanceOf.call(addr).then(function(b){
+        balance.token = b.c[0];
+        res.send(balance);
+    });
 });
